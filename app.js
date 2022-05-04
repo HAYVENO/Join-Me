ef21ad31fba7a6e048c9b47910fd577c9478e397
Abdulhafeez Yusuf <haythepen@gmail.com>
2022-05-04 15:15:10 +0100

    :rocket: first commit

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+const app = express();
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mailchimp.setConfig({
+  apiKey: "cbb9e42bf9789ca015bd05a2b90b8c75-us11",
+  server: "us11",
+});
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  const fName = req.body.fName;
+  const lName = req.body.lName;
+  const Email = req.body.Email;
+  console.log(fName, lName, Email);
+
+  const listId = "329c35e482";
+  const subscribingUser = {
+    firstName: fName,
+    lastName: lName,
+    email: Email,
+  };
+
+  const run = async () => {
+    try {
+      const response = await mailchimp.lists.addListMember(listId, {
+        email_address: subscribingUser.email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: subscribingUser.firstName,
+          LNAME: subscribingUser.lastName,
+        },
+      });
+      console.log(response);
+      res.sendFile(__dirname + "/success.html");
+    } catch (err) {
+      console.log(err.status);
+      res.sendFile(__dirname + "/failure.html");
+    }
+  };
+
+  run();
+});
+
+app.listen(1000, function () {
+  console.log("Newsletter  server UP on port 3000: ACCESS GRANTED");
+});
+
+// API Mailchimp
+// cbb9e42bf9789ca015bd05a2b90b8c75 - us11;
+
+// audience ID (List ID)
+// 329c35e482
