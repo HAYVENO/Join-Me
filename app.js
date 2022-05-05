0a59a72cdb89321bc74974a3ec7924caaf6fbf15
Abdulhafeez Yusuf <haythepen@gmail.com>
2022-05-05 18:46:52 +0100

    proc

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const https = require("https");
 const app = express();
 const mailchimp = require("@mailchimp/mailchimp_marketing");
 
