7e8ddbd9c65950a8af16b43d99679a1b03a19d1f
Abdulhafeez Yusuf <haythepen@gmail.com>
2022-05-14 22:44:19 +0100

    :rocket: image updated

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const https = require("https");
+// const https = require("https");
 const app = express();
 const mailchimp = require("@mailchimp/mailchimp_marketing");
 
@@ -9,50 +9,50 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 mailchimp.setConfig({
-  apiKey: "cbb9e42bf9789ca015bd05a2b90b8c75-us11",
-  server: "us11",
+	apiKey: "cbb9e42bf9789ca015bd05a2b90b8c75-us11",
+	server: "us11",
 });
 
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/signup.html");
+	res.sendFile(__dirname + "/signup.html");
 });
 
 app.post("/", function (req, res) {
-  const fName = req.body.fName;
-  const lName = req.body.lName;
-  const Email = req.body.Email;
-  console.log(fName, lName, Email);
+	const fName = req.body.fName;
+	const lName = req.body.lName;
+	const Email = req.body.Email;
+	console.log(fName, lName, Email);
 
-  const listId = "329c35e482";
-  const subscribingUser = {
-    firstName: fName,
-    lastName: lName,
-    email: Email,
-  };
+	const listId = "329c35e482";
+	const subscribingUser = {
+		firstName: fName,
+		lastName: lName,
+		email: Email,
+	};
 
-  const run = async () => {
-    try {
-      const response = await mailchimp.lists.addListMember(listId, {
-        email_address: subscribingUser.email,
-        status: "subscribed",
-        merge_fields: {
-          FNAME: subscribingUser.firstName,
-          LNAME: subscribingUser.lastName,
-        },
-      });
-      console.log(response.full_name);
-      res.sendFile(__dirname + "/success.html");
-    } catch (err) {
-      console.log(err.status);
-      res.sendFile(__dirname + "/failure.html");
-    }
-  };
+	const run = async () => {
+		try {
+			const response = await mailchimp.lists.addListMember(listId, {
+				email_address: subscribingUser.email,
+				status: "subscribed",
+				merge_fields: {
+					FNAME: subscribingUser.firstName,
+					LNAME: subscribingUser.lastName,
+				},
+			});
+			console.log(response.full_name);
+			res.sendFile(__dirname + "/success.html");
+		} catch (err) {
+			console.log(err.status);
+			res.sendFile(__dirname + "/failure.html");
+		}
+	};
 
-  run();
+	run();
 });
 
-app.listen(process.env.PORT || 1000, function () {
-  console.log("Newsletter  server UP on port 1000: ACCESS GRANTED");
+app.listen(process.env.PORT || 6000, function () {
+	console.log("Newsletter  server UP on port 6000: ACCESS GRANTED");
 });
 
 // let port = process.env.PORT;
